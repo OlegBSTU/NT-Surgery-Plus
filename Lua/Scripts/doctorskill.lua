0ec62a94e230f80e262e40120aeedb5d3d9b3793
Echo <github@drunken-dwarf.de>
2025-06-08 21:14:31 +0200

    Removed unnecessary argument for SetSkillLevel + added empty line at end of file

--- Lua/Scripts/doctorskill.lua
+++ Lua/Scripts/doctorskill.lua
@@ -2,7 +2,7 @@ local skillAmountToAdd = math.random(20, 25)
 
 local function addSurgerySkillToDoctors(character)
 	if character ~= nil and character.Info.Job.Prefab.Identifier.Value == "medicaldoctor" and character.GetSkillLevel("surgery") <= 20 then
-		character.Info.SetSkillLevel("surgery", skillAmountToAdd, false)
+		character.Info.SetSkillLevel("surgery", skillAmountToAdd)
 	end
 end
 
